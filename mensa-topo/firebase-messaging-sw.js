0ed803ed175d24eaee8020e7876bc6d3dfc1ff2b
campitos <rapidclimate@gmail.com>
2019-08-28 11:29:06 -0500

    Auto-generated commit

---
+++ mensa-topo/firebase-messaging-sw.js
@@ -0,0 +1,22 @@
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBzTK4rDf3XT-r_waqm46gfYsJdRR813dU",
+  authDomain: "salud-unitec.firebaseapp.com",
+  databaseURL: "https://salud-unitec.firebaseio.com",
+  projectId: "salud-unitec",
+  storageBucket: "salud-unitec.appspot.com",
+  messagingSenderId: "181953839266",
+  appId: "1:181953839266:web:9cfb0b9141887ef9"
+};
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+messaging.setBackgroundMessageHandler(function(payload) {
+    const title = 'Hello World from SW!';
+    const options = {
+        body: payload.data.status
+    };
+    return self.registration.showNotification(title, options);
+});
