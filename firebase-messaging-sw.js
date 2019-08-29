c6c78514c5838ca0c4cd865b741ef068dbe46878
campitos <rapidclimate@gmail.com>
2019-08-28 19:16:24 -0500

    Auto-generated commit

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,9 +3,13 @@ importScripts('https://www.gstatic.com/firebasejs/6.3.4/firebase-auth.js');
 importScripts('https://www.gstatic.com/firebasejs/6.3.4/firebase-messaging.js');
 
 var firebaseConfig = {
-
+  apiKey: "AIzaSyBzTK4rDf3XT-r_waqm46gfYsJdRR813dU",
+  authDomain: "salud-unitec.firebaseapp.com",
+  databaseURL: "https://salud-unitec.firebaseio.com",
+  projectId: "salud-unitec",
+  storageBucket: "salud-unitec.appspot.com",
   messagingSenderId: "181953839266",
-
+  appId: "1:181953839266:web:9cfb0b9141887ef9"
 };
 firebase.initializeApp(firebaseConfig );
 
